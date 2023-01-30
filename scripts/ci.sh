6c8ad66fd6b3f9d89eb8f557b459afc202704b3e
eigmax <stephen@ieigen.com>
2023-01-31 00:19:15 +0800

    update example

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,14 +3,15 @@ set -xe
 cmd="node --max-old-space-size=32000"
 
 #pil-stark source directory
-git clone https://github.com/eigmax/pil-stark
-cd pil-stark
+npm i
+cd node_modules/pil-stark
 #curdir=$(cd $(dirname $0)/..; pwd)
 curdir=$PWD
 
+#for debug, you can skip the circom compile
 if [ 0 = 0 ]; then
 
-rm -rf ${curdir}/tmp/*
+mkdir -p ${curdir}/tmp && rm -rf ${curdir}/tmp/*
 
 ${cmd} ${curdir}/test/sm_fibonacci/main_buildconst_fibonacci.js \
     -o ${curdir}/tmp/fibonacci.const.bin
@@ -31,32 +32,38 @@ ${cmd} src/main_buildconsttree.js \
     -t ${curdir}/tmp/fibonacci.consttree \
     -v ${curdir}/tmp/fibonacci.verkey.json
 
+# starkSetup
+${cmd} src/main_genstarkinfo.js \
+    -p ${curdir}/test/sm_fibonacci/fibonacci_main.pil \
+    -s ${curdir}/test/sm_fibonacci/fibonacci.starkstruct.json \
+    -i ${curdir}/tmp/fibonacci.starkinfo.json
+
 ${cmd} src/main_prover.js -m ${curdir}/tmp/fibonacci.commit \
     -c ${curdir}/tmp/fibonacci.const.bin \
     -t ${curdir}/tmp/fibonacci.consttree \
     -p ${curdir}/test/sm_fibonacci/fibonacci_main.pil \
-    -s ${curdir}/test/sm_fibonacci/fibonacci.starkstruct.json \
+    -s ${curdir}/tmp/fibonacci.starkinfo.json \
     -o ${curdir}/tmp/fibonacci.proof.json \
     -z ${curdir}/tmp/fibonacci.proof.zkin.json \
     -b ${curdir}/tmp/fibonacci.public.json
 
 ${cmd} src/main_verifier.js \
     -p ${curdir}/test/sm_fibonacci/fibonacci_main.pil \
-    -s ${curdir}/test/sm_fibonacci/fibonacci.starkstruct.json \
+    -s ${curdir}/tmp/fibonacci.starkinfo.json \
     -o ${curdir}/tmp/fibonacci.proof.json \
     -b ${curdir}/tmp/fibonacci.public.json \
     -v ${curdir}/tmp/fibonacci.verkey.json
 
 ${cmd} src/main_pil2circom.js \
     -p ${curdir}/test/sm_fibonacci/fibonacci_main.pil \
-    -s ${curdir}/test/sm_fibonacci/fibonacci.starkstruct.json \
+    -s ${curdir}/tmp/fibonacci.starkinfo.json \
     -v ${curdir}/tmp/fibonacci.verkey.json \
     -o ${curdir}/tmp/fibonacci.verifier.circom
 
-cp ${curdir}/tmp/fibonacci.verifier.circom ${curdir}/../eigen-zkit/fri_verifier/circuits
-cp ${curdir}/tmp/fibonacci.proof.zkin.json ${curdir}/../eigen-zkit/test
-
-yarn fibonacci_compileverifier
+circom --r1cs --wasm -p goldilocks ${curdir}/tmp/fibonacci.verifier.circom \
+   -l circuits.gl \
+   --O2=full \
+   -o ${curdir}/tmp/
 
 fi
 
@@ -78,6 +85,7 @@ ${cmd} src/compressor12/main_compressor12_exec.js \
     -e ${curdir}/tmp/fibonacci.c12.exec \
     -m ${curdir}/tmp/fibonacci.c12.commit
 
+# Note: maybe need to update the nBits in c12.starkstruct.json
 ${cmd} src/main_buildconsttree.js \
     -p ${curdir}/tmp/fibonacci.c12.pil \
     -c ${curdir}/tmp/fibonacci.c12.const \
@@ -90,7 +98,7 @@ ${cmd} src/main_prover.js \
     -c ${curdir}/tmp/fibonacci.c12.const \
     -t ${curdir}/tmp/fibonacci.c12.consttree \
     -p ${curdir}/tmp/fibonacci.c12.pil \
-    -s ${curdir}/test/sm_fibonacci/fibonacci.c12.starkstruct.json \
+    -s ${curdir}/tmp/fibonacci.c12.starkinfo.json \
     -o ${curdir}/tmp/fibonacci.c12.proof.json \
     -z ${curdir}/tmp/fibonacci.c12.proof.zkin.json \
     -b ${curdir}/tmp/fibonacci.c12.public.json \
@@ -98,7 +106,7 @@ ${cmd} src/main_prover.js \
 
 ${cmd} src/main_verifier.js \
     -p ${curdir}/tmp/fibonacci.c12.pil \
-    -s ${curdir}/test/sm_fibonacci/fibonacci.c12.starkstruct.json \
+    -s ${curdir}/tmp/fibonacci.c12.starkinfo.json \
     -o ${curdir}/tmp/fibonacci.c12.proof.json \
     -b ${curdir}/tmp/fibonacci.c12.public.json \
     -v ${curdir}/tmp/fibonacci.c12.verkey.json
